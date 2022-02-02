419add392691422d408e54d637669a33b66cdc2d
Calamity34 <NikitaYoutube2008@icloud.com>
2022-02-02 21:31:06 +0500

    username feature 1

--- anonchat-cli/bin/anonchat_cli.dart
+++ anonchat-cli/bin/anonchat_cli.dart
@@ -3,21 +3,24 @@ import 'dart:io';
 
 Future<void> main(List<String> arguments) async {
   if (arguments.isEmpty) {
-    print('Usage: anonchat-cli <host:port>');
+    print('Usage: anonchat-cli <host:port> <username>'); // TODO: make username optional, default to Anon
     return;
   }
+  
+  // Assign variables
+  String ip = arguments[0].split(':')[0];
+  int port = int.parse(arguments[0].split(':')[1]);
+  String user = arguments[1];
 
   // Connect to the server
-  final socket = await Socket.connect(
-    arguments[0].split(':')[0],
-    int.parse(arguments[0].split(':')[1]),
-  );
+  final socket = await Socket.connect(ip,port);
+  print('Connected to $ip at $port as $user');
 
   // Send message from input
-  stdin.pipe(socket);
+  stdin.listen((v) => socket.add(utf8.encode('<$user> ') + v));
 
   // Print new messages
   socket.listen(
-    (v) => print('> ' + utf8.decode(v, allowMalformed: true).trim()),
+    (v) => print(utf8.decode(v, allowMalformed: true).trim()),
   );
 }
