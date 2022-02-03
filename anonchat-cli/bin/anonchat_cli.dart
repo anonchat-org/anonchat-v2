e3a6d651aff856483f0f1743d727a1e569174d71
Calamity34 <NikitaYoutube2008@icloud.com>
2022-02-03 12:01:36 +0500

    made username system

--- anonchat-cli/bin/anonchat_cli.dart
+++ anonchat-cli/bin/anonchat_cli.dart
@@ -3,18 +3,23 @@ import 'dart:io';
 
 Future<void> main(List<String> arguments) async {
   if (arguments.isEmpty) {
-    print('Usage: anonchat-cli <host:port> <username>'); // TODO: make username optional, default to Anon
+    print('Usage: anonchat-cli <host:port> [username]');
     return;
   }
   
   // Assign variables
   String ip = arguments[0].split(':')[0];
   int port = int.parse(arguments[0].split(':')[1]);
-  String user = arguments[1];
-
+  String user;
+  if(arguments.length<=1) {
+    user = 'Anon';
+  } else {
+    user = arguments[1];
+  }
+    
   // Connect to the server
   final socket = await Socket.connect(ip,port);
-  print('Connected to $ip at $port as $user');
+  print('\/\/ Connected to $ip at $port as $user\n');
 
   // Send message from input
   stdin.listen((v) => socket.add(utf8.encode('<$user> ') + v));
