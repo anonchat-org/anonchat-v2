ac2a7fc0125115ea75744761a070318e989e4473
KrutosX <52695559+KrutosVIP@users.noreply.github.com>
2022-02-03 20:14:51 +0400

    Patch build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 dart format anonchat-cli/bin/anonchat_cli.dart
 dart format anonchat-server/bin/anonchat_server.dart
-rm build/*
+rm -rf build
+mkdir build
 dart compile exe anonchat-cli/bin/anonchat_cli.dart -o build/cli
 dart compile exe anonchat-server/bin/anonchat_server.dart -o build/server
+chmod +x build/*
